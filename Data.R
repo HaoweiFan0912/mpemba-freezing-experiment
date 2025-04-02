5a00624e813a75cddc9e59e2e1a3ae50a847efe6
HaoweiFan0912 <fhw0912@gmail.com>
2025-04-01 21:35:51 -0400

    codes styled, readme added
    codes styled, readme added

--- Data.R
+++ Data.R
@@ -8,25 +8,37 @@ install.packages(missing_packages)
 # Load packages
 lapply(packages, library, character.only = TRUE)
 
-# 
+#
 freeze <- data.frame(
   experimental_condition = 1:8,
-  
+
   # Factor A: Initial water temperature (Room temperature vs. Hot).
-  water_temp = factor(c("RoomTemp", "RoomTemp", "RoomTemp", "RoomTemp",
-                        "Hot", "Hot", "Hot", "Hot"), 
-                      levels = c("RoomTemp", "Hot")),
-  
+  water_temp = factor(
+    c(
+      "RoomTemp", "RoomTemp", "RoomTemp", "RoomTemp",
+      "Hot", "Hot", "Hot", "Hot"
+    ),
+    levels = c("RoomTemp", "Hot")
+  ),
+
   # Factor B: Sugar added (No vs. Yes).
-  sugar = factor(c("NoSugar", "NoSugar", "Sugar", "Sugar",
-                   "NoSugar", "NoSugar", "Sugar", "Sugar"),
-                 levels = c("NoSugar", "Sugar")),
-  
+  sugar = factor(
+    c(
+      "NoSugar", "NoSugar", "Sugar", "Sugar",
+      "NoSugar", "NoSugar", "Sugar", "Sugar"
+    ),
+    levels = c("NoSugar", "Sugar")
+  ),
+
   # Factor C: Cup material (Plastic vs. Glass).
-  cup_material = factor(c("Plastic", "Glass", "Plastic", "Glass",
-                          "Plastic", "Glass", "Plastic", "Glass"),
-                        levels = c("Plastic", "Glass")),
-  
+  cup_material = factor(
+    c(
+      "Plastic", "Glass", "Plastic", "Glass",
+      "Plastic", "Glass", "Plastic", "Glass"
+    ),
+    levels = c("Plastic", "Glass")
+  ),
+
   # Observed freezing time in minutes.
   time_1 = c(135, 120, 150, 140, 165, 145, 175, 155),
   time_2 = c(135, 125, 155, 135, 165, 140, 170, 165)
